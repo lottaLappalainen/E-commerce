53f7821ae7adb7fa7ae78212eec917e29086e041
Lotta <lotta.lappalainen@tuni.fi>
2026-03-03 13:00:45 +0200

    add mapping configuration to program.cs

--- Ecommerce.Api/Program.cs
+++ Ecommerce.Api/Program.cs
@@ -2,6 +2,7 @@ using System.Text;
 using Ecommerce.Api.Data;
 using Ecommerce.Api.Middleware;
 using Ecommerce.Api.Services;
+using Ecommerce.Api.Mappings; 
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.RateLimiting;
 using Microsoft.EntityFrameworkCore;
@@ -10,31 +11,41 @@ using System.Threading.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// PostgreSQL
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(
         builder.Configuration.GetConnectionString("DefaultConnection")
     ));
 
+
+// controllers ja swagger
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// automapper
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+// services
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 
+// login rajotus
 builder.Services.AddRateLimiter(options =>
 {
     options.AddFixedWindowLimiter("loginPolicy", opt =>
     {
         opt.Window = TimeSpan.FromMinutes(1);
-        opt.PermitLimit = 5; // max 5 login yritystä / min
+        opt.PermitLimit = 5;
         opt.QueueLimit = 0;
     });
 });
 
-var jwtKey = builder.Configuration["Jwt:Key"]!;
+// JWT authentication
+var jwtKey = builder.Configuration["Jwt:Key"]
+    ?? throw new Exception("JWT Key not configured.");
 
 builder.Services.AddAuthentication(options =>
 {
@@ -56,8 +67,10 @@ builder.Services.AddAuthentication(options =>
 
 builder.Services.AddAuthorization();
 
+// build app
 var app = builder.Build();
 
+// global middleware
 app.UseMiddleware<ExceptionMiddleware>();
 
 if (app.Environment.IsDevelopment())
